3b38f6800f511e092e7d955eaf4c37c5e4c0bcc0
RH2026 <rigobertto.hernandez@gmail.com>
2025-12-25 15:22:58 -0600

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -40,7 +40,6 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-
 # --------------------------------------------------
 # CARGA DE DATOS
 # --------------------------------------------------
@@ -181,6 +180,7 @@ st.markdown(
     "<div style='text-align:center; color:gray; margin-top:20px;'>© 2026 Logística – Control de Envios</div>",
     unsafe_allow_html=True
 )
+
 
 
 
