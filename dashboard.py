ebbff539e2ede48a0d4d1c849ea33bbf3ffd17fb
RH2026 <rigobertto.hernandez@gmail.com>
2025-12-25 15:17:13 -0600

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -14,7 +14,7 @@ st.markdown(
     <div style='text-align:center;'>
         <div style='
             color:white;
-            font-size:56px;
+            font-size:32px;
             font-weight:700;
         '>
             Panel de Seguimiento y Control de Embarques
@@ -172,6 +172,7 @@ st.markdown(
     "<div style='text-align:center; color:gray; margin-top:20px;'>© 2026 Logística – Control de Envios</div>",
     unsafe_allow_html=True
 )
+
 
 
 
