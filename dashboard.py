98db3271542e3521a4447309881f0df3ccbfb227
RH2026 <rigobertto.hernandez@gmail.com>
2025-12-26 02:01:48 -0600

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -158,10 +158,10 @@ if pedido_buscar.strip() != "":
             <div style='background-color:#2b2b2b; padding:15px; border-radius:10px;'>
                 <div style='color:yellow; font-size:16px; font-weight:bold; margin-bottom:10px; text-align:center;'>Estatus y Observaciones</div>
                 <b>Estatus:</b> {row['ESTATUS_CALCULADO']}<br>
-                <b>Estatus:</b> {row['CLASES DE ENTREGA']}<br>                
+                <b>Clase de Entrega:</b> {row['CLASES DE ENTREGA']}<br>                
                 <b>Prioridad:</b> {row.get('PRIORIDAD', '')}<br>
                 <b>Comentarios:</b> {row.get('COMENTARIOS', '')}<br>
-                <b>Estatus:</b> {row['CANTIDAD DE CAJAS']}<br>                
+                <b>Cantidad de Cajas:</b> {row['CANTIDAD DE CAJAS']}<br>                
             </div>
             """,
             unsafe_allow_html=True
@@ -514,6 +514,7 @@ st.markdown(
     "<div style='text-align:center; color:gray; margin-top:20px;'>© 2026 Logística – Control de Envios</div>",
     unsafe_allow_html=True
 )
+
 
 
 
