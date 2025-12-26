15097690100f30eb52973afcd1b4271ecfda05ee
RH2026 <rigobertto.hernandez@gmail.com>
2025-12-26 02:05:51 -0600

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -249,7 +249,7 @@ st.markdown(
     """
     <div style="text-align:center;">
         <div style="color:white; font-size:24px; font-weight:700; margin:10px 0;">
-            Indicadores Clave
+            Indicadores Generales
         </div>
     </div>
     """,
@@ -514,6 +514,7 @@ st.markdown(
     "<div style='text-align:center; color:gray; margin-top:20px;'>© 2026 Logística – Control de Envios</div>",
     unsafe_allow_html=True
 )
+
 
 
 
