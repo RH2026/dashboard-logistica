f47db7bf46c8abbaca8b4370ba2c79e7c458c458
RH2026 <rigobertto.hernandez@gmail.com>
2025-12-27 01:27:58 -0600

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -208,7 +208,7 @@ if st.session_state.logueado:
     # --- FILTRO FLETERA (solo una para gráficos) ---
     fletera_sel = st.sidebar.selectbox(
         "Fletera",
-        options=["Prueba"] + sorted(df["FLETERA"].dropna().unique()),  # "" permite no seleccionar nada
+        options=[""] + sorted(df["FLETERA"].dropna().unique()),  # "" permite no seleccionar nada
         index=0
     )
     
@@ -731,6 +731,7 @@ if st.session_state.logueado:
         "<div style='text-align:center; color:gray; margin-top:20px;'>© 2026 Logística – Control de Envios</div>",
         unsafe_allow_html=True
     )
+
 
 
 
