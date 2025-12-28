4f710d2f2ccf8b24fe4e591b6c890a77b3e0af3b
RH2026 <rigobertto.hernandez@gmail.com>
2025-12-28 02:44:01 -0600

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -377,15 +377,15 @@ if st.session_state.logueado:
                     </div>
                 """, unsafe_allow_html=True)
         
-                    st.markdown(f"""
-                        <div style='{estilo_card}'>
-                            <div style='color:yellow; font-weight:bold; text-align:center; margin-bottom:10px;'>Fechas y Seguimiento</div>
-                            <b>FECHA DE ENVÍO:</b> {f_envio}<br>
-                            <b>PROMESA ENTREGA:</b> {f_prom}<br>
-                            <b>FECHA DE ENTREGA REAL:</b> {f_real}<br><br>
-                            <b>DÍAS RETRASO:</b> <span style='color:{color_retraso}; font-weight:bold;'>{retraso}</span>
-                        </div>
-                    """, unsafe_allow_html=True)
+                st.markdown(f"""
+                    <div style='{estilo_card}'>
+                        <div style='color:yellow; font-weight:bold; text-align:center; margin-bottom:10px;'>Fechas y Seguimiento</div>
+                        <b>FECHA DE ENVÍO:</b> {f_envio}<br>
+                        <b>PROMESA ENTREGA:</b> {f_prom}<br>
+                        <b>FECHA DE ENTREGA REAL:</b> {f_real}<br><br>
+                        <b>DÍAS RETRASO:</b> <span style='color:{color_retraso}; font-weight:bold;'>{retraso}</span>
+                    </div>
+                """, unsafe_allow_html=True)
     
             with c3:
                 st.markdown(f"""
@@ -773,6 +773,7 @@ if st.session_state.logueado:
         "<div style='text-align:center; color:gray; margin-top:20px;'>© 2026 Logística – Control de Envios</div>",
         unsafe_allow_html=True
     )
+
 
 
 
