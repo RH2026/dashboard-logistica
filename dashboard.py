ff0c42baaa50ed970558c530d31fd88ce5983566
RH2026 <rigobertto.hernandez@gmail.com>
2025-12-28 00:41:33 -0600

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -445,7 +445,7 @@ if st.session_state.logueado:
             </div>
             </div>
             """),
-                unsafe_allow_html=True
+            unsafe_allow_html=True
             )
     
     # --------------------------------------------------
@@ -812,6 +812,7 @@ if st.session_state.logueado:
         "<div style='text-align:center; color:gray; margin-top:20px;'>© 2026 Logística – Control de Envios</div>",
         unsafe_allow_html=True
     )
+
 
 
 
