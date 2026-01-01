c4907cb55aca0e071ac336e6abaf239484532883
Rigobertto Hernandez <rigobertto.hernandez@gmail.com>
2025-12-31 21:34:44 -0600

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -30,7 +30,7 @@ st.markdown("""
     <style>
         .block-container {
             padding-top: 1.5rem !important;
-            padding-bottom: 0rem !important;
+            padding-bottom: 1.5rem !important;
             padding-left: 2rem !important;   /* Siempre ancho total */
             padding-right: 2rem !important;  /* Siempre ancho total */
         }
@@ -887,6 +887,7 @@ else:
             st.rerun()
     
         st.markdown("<div style='text-align:center; color:gray; margin-top:20px;'>© 2026 Vista Gerencial</div>", unsafe_allow_html=True)
+
 
 
 
