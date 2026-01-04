e81eae4a3642f9c103c934afaf23bf5a2c2bbcec
Rigobertto Hernandez <rigobertto.hernandez@gmail.com>
2026-01-03 19:05:48 -0600

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1362,7 +1362,7 @@ else:
             mes_sel = st.sidebar.selectbox("MES ACTUAL / BASE", meses_limpios)
             df_mes = df_a[df_a["MES"] == mes_sel].iloc[0]
             
-            modo_comp = st.sidebar.checkbox("🚀 ACTIVAR COMPARATIVA MES VS MES")
+            modo_comp = st.sidebar.checkbox("Activar comparativa Mes vs Mes")
             if modo_comp:
                 mes_comp = st.sidebar.selectbox("COMPARAR CONTRA:", meses_limpios, index=0)
                 df_mes_b = df_a[df_a["MES"] == mes_comp].iloc[0]
@@ -1385,7 +1385,7 @@ else:
             """, unsafe_allow_html=True)
         
             header_txt = f"Resultados: {mes_sel}" if not modo_comp else f"Comparativa Mode: {mes_sel} VS {mes_comp}"
-            st.markdown(f"<h3 class='premium-header'>{header_txt}</h3>", unsafe_allow_html=True)
+            st.markdown(f"<h4 class='premium-header'>{header_txt}</h4>", unsafe_allow_html=True)
         
             if not modo_comp:
                 # --- VISTA NORMAL 9 TARJETAS ---
@@ -1499,6 +1499,7 @@ else:
                 st.rerun()
 
         st.markdown("<div style='text-align:center; color:#475569; font-size:10px; margin-top:20px;'>LOGISTICS INTELLIGENCE UNIT - CONFIDENTIAL</div>", unsafe_allow_html=True)
+
 
 
 
