582c9a7137dd5ebafbf6214db44a5e7e308d3db0
Rigobertto Hernandez <rigobertto.hernandez@gmail.com>
2026-01-06 17:35:17 -0600

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1651,6 +1651,17 @@ else:
             header_txt = f"Resultados: {mes_sel}" if not modo_comp else f"Comparativa Mode: {mes_sel} VS {mes_comp}"
             st.markdown(f"<h4 class='premium-header'>{header_txt}</h4>", unsafe_allow_html=True)
 
+                st.markdown("""
+                <hr style="
+                    border: 0;
+                    height: 2px;
+                    background: #00D4FF;
+                    box-shadow: 0px 0px 15px 2px rgba(0, 212, 255, 0.7);
+                    margin-top: 20px;
+                    margin-bottom: 20px;
+                    border-radius: 10px;
+                ">
+                """, unsafe_allow_html=True)
                         
             if not modo_comp:
                 # --- VISTA NORMAL 9 TARJETAS ---
@@ -1669,19 +1680,7 @@ else:
                 with c8: render_card("Cajas Enviadas", f"{int(df_mes['CAJAS ENVIADAS']):,.0f}", "Volumen", border_base="border-purple")
                 with c9: render_card("Costo de Flete", f"${df_mes['COSTO DE FLETE']:,.0f}", "Inversión", border_base="border-blue")
                 
-                
-                st.markdown("""
-                <hr style="
-                    border: 0;
-                    height: 2px;
-                    background: #00D4FF;
-                    box-shadow: 0px 0px 15px 2px rgba(0, 212, 255, 0.7);
-                    margin-top: 20px;
-                    margin-bottom: 20px;
-                    border-radius: 10px;
-                ">
-                """, unsafe_allow_html=True)
-             
+                                             
                 
                 # --- BLOQUE PREMIUM DE CÁLCULOS ---
                 st.markdown(f"""
@@ -2132,6 +2131,7 @@ else:
                
         st.markdown('</div>', unsafe_allow_html=True)
         st.markdown("<div style='text-align:center; color:#475569; font-size:10px; margin-top:20px;'>LOGISTICS INTELLIGENCE UNIT - CONFIDENTIAL</div>", unsafe_allow_html=True)
+
 
 
 
